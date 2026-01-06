eb3dc02f1a003061591cc136d538a6439be1d4d5
kelvin-brandia <kelvimgomes86@gmail.com>
2026-01-06 18:28:57 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,11 @@
-import type { ReactNode } from "react";
+import { ReactNode } from "react";
 
-export default function RootLayout({
-  children,
-}: {
-  children: ReactNode;
-}) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="pt-BR">
-      <body>{children}</body>
+      <body>
+        {children} {/* Isso é obrigatório para o page.tsx aparecer */}
+      </body>
     </html>
   );
 }

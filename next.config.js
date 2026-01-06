0ea7544f282a70a8a9be06c58562d09e5e007c92
kelvin-brandia <kelvimgomes86@gmail.com>
2026-01-06 20:28:00 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // Permite que a build complete mesmo se houver erros de TypeScript
     ignoreBuildErrors: true,
   },
   eslint: {
-    // Também ignora erros de linting para garantir a build
     ignoreDuringBuilds: true,
   },
 }

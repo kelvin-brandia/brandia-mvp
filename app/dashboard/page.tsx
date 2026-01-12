8da276c9c5a4195e291b236761c8086d89dd3b63
kelvin-brandia <kelvimgomes86@gmail.com>
2026-01-12 15:32:45 -0300

    Create page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { supabase } from '@/lib/supabase'
+import { supabase } from '../../lib/supabase'
 import { useEffect } from 'react'
 import { useRouter } from 'next/navigation'
 

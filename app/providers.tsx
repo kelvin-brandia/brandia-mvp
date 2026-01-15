e3006aaf3fbd4cdba36fafba7c23d78452e9acc7
kelvin-brandia <kelvimgomes86@gmail.com>
2026-01-15 15:46:50 -0300

    Create providers.tsx

---
+++ app/providers.tsx
@@ -0,0 +1,15 @@
+import Providers from "./providers";
+
+export const dynamic = "force-dynamic";
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="pt-BR">
+      <body>
+        <Providers>
+          {children}
+        </Providers>
+      </body>
+    </html>
+  );
+}
